d4452c49c489510667542e6adf065b6d808c020d
Andrew Reeves <areeves42@gmail.com>
2022-08-23 16:08:19 -0400

    Build tweak for github

--- config/index.js
+++ config/index.js
@@ -50,7 +50,10 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+
+    // @TODO inject this from github workflow
+    //assetsPublicPath: '/',
+    assetsPublicPath: '/vue-games',
 
     /**
      * Source Maps
